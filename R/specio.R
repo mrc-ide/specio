8044d840ba6a4c8e1b5360d0c25388669aea7ca0
Robert Ashton <r.ashton@imperial.ac.uk>
2019-01-04 15:01:24 +0000

    Add documentation

--- R/specio.R
+++ R/specio.R
@@ -1,3 +1,20 @@
+
+
+#' @title Add two numbers.
+#'
+#' This is the description section (one paragraph)
+#'
+#' This is the details section (zero or more paragraphs)
+#'
+#' @param a First number to add
+#' @param b Second number to add
+#'
+#' @return The added numbers
+#' @export
+#'
+#' @examples
+#' add(1, 2)
+#'
 add <- function(a, b) {
   a + b
 }
