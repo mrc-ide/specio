449dc87f411e884ba4a01b25185f7ec0aedd36be
Robert Ashton <r.ashton@imperial.ac.uk>
2019-03-08 16:00:21 +0000

    Fix typo

--- R/model_params.R
+++ R/model_params.R
@@ -4,7 +4,7 @@ get_model_params <- function() {
     AG = 17,
     DS = 7,
     TS = 3,
-    fAG = 7,    # fertile age groupsde
+    fAG = 7,    # fertile age groups
     PAED_DS = 6 # number of paediatric stages of infection
   )
 }
