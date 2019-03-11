2f273d7cc3daf412c3e6ac47f7c66c6d335b9707
Robert Ashton <r.ashton@imperial.ac.uk>
2019-03-11 09:36:09 +0000

    Ensure fallback functions are tested

--- tests/testthat/test_read_hivproj_param.R
+++ tests/testthat/test_read_hivproj_param.R
@@ -32,3 +32,25 @@ test_that("scale_cd4_mortality can be interpreted", {
   version <- "5.72 Beta 16"
   expect_equal(get_scale_cd4_mortality(version), 0L)
 })
+
+test_that("women on ART uses fallback function to return default values", {
+  women_on_art <- get_property_data("women_on_art", dp_data)
+  expect_equal(
+    women_on_art,
+    setNames(
+      rep(1.0, 7),
+      c("15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49")
+    )
+  )
+})
+
+test_that("ART allocation uses fallback function to return default values", {
+  art_prop_alloc <- get_property_data("art_prop_alloc", dp_data)
+  expect_equal(
+    art_prop_alloc,
+    stats::setNames(
+      c(0.5, 0.5),
+      c("mx", "elig")
+    )
+  )
+})
