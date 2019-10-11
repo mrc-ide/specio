e5c14e9bdb12f24d1f137f858a00bfc8e166e71b
Robert Ashton <r.ashton@imperial.ac.uk>
2019-10-11 12:42:42 +0100

    Fix partial string match

--- tests/testthat/helper_xml_utils.R
+++ tests/testthat/helper_xml_utils.R
@@ -1,3 +1,3 @@
-files <- dir("xml_testdata", full.name = TRUE)
+files <- dir("xml_testdata", full.names = TRUE)
 xml_testdata <- lapply(files, xml2::read_xml)
 names(xml_testdata) <- sub(".xml$", "", basename(files))
